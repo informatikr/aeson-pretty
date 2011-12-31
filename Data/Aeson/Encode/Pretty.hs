5e016aa177f33c4e286fd395bad6c3698e9edf91
Falko Peters <falko.peters@googlemail.com>
2011-12-31 17:32:37 +0100

    Bump outdated dependencies, notably to aeson 0.5.0.*. Internally, the lazy Text Builder is used instead of Blaze Builder.

--- Data/Aeson/Encode/Pretty.hs
+++ Data/Aeson/Encode/Pretty.hs
@@ -3,8 +3,6 @@
 -- |Aeson-compatible pretty-printing of JSON 'Value's.
 module Data.Aeson.Encode.Pretty (encodePretty) where
 
-import Blaze.ByteString.Builder (Builder, toLazyByteString, fromByteString)
-import Blaze.ByteString.Builder.Char.Utf8 (fromChar)
 import Data.Aeson (Value(..), ToJSON(..))
 import qualified Data.Aeson.Encode as Aeson
 import Data.ByteString.Lazy (ByteString)
@@ -12,6 +10,8 @@ import qualified Data.HashMap.Strict as H (toList)
 import Data.List (intersperse)
 import Data.Monoid (mappend, mconcat, mempty)
 import Data.Text (Text)
+import Data.Text.Lazy.Builder
+import Data.Text.Lazy.Encoding (encodeUtf8)
 import qualified Data.Vector as V (toList)
 
 
@@ -20,41 +20,37 @@ type Indent = Int
 -- |A drop-in replacement for aeson's 'Aeson.encode' function, producing 
 --  JSON-ByteStrings for human readers.
 encodePretty :: ToJSON a => a -> ByteString
-encodePretty = toLazyByteString . fromValue 0 . toJSON
+encodePretty = encodeUtf8 . toLazyText . fromValue 0 . toJSON
 
 fromValue :: Indent -> Value -> Builder
 fromValue ind = go
   where
-    go (Array v)  = fromCompound ind ('[',']') fromValue (V.toList v)
-    go (Object m) = fromCompound ind ('{','}') fromPair (H.toList m)
+    go (Array v)  = fromCompound ind ("[","]") fromValue (V.toList v)
+    go (Object m) = fromCompound ind ("{","}") fromPair (H.toList m)
     go v          = Aeson.fromValue v
 
 fromCompound :: Indent
-             -> (Char, Char)
+             -> (Builder, Builder)
              -> (Indent -> a -> Builder)
              -> [a]
              -> Builder
-fromCompound ind (delimL,delimR) fromItem items =    
-    fromChar delimL `mappend` items' `mappend` fromChar delimR
+fromCompound ind (delimL,delimR) fromItem items =
+    delimL `mappend` items' `mappend` delimR
   where
-    newLine = fromChar '\n'
     items'  = if null items then mempty
                 else mconcat
-                    [ newLine
-                    , mconcat . intersperse (fromChar ',' `mappend` newLine) $
+                    [ "\n"
+                    , mconcat . intersperse ",\n" $
                         map (\i -> fromIndent (ind+1) `mappend`
                                    fromItem (ind+1) i)
                             items
-                    , newLine
+                    , "\n"
                     , fromIndent ind
                     ]
 
 fromPair :: Indent -> (Text, Value) -> Builder
 fromPair ind (k,v) =
-    mconcat [ Aeson.fromValue (toJSON k)
-            , fromByteString ": "
-            , fromValue ind v
-            ]
+    Aeson.fromValue (toJSON k) `mappend` ": " `mappend` fromValue ind v
 
 fromIndent :: Indent -> Builder
-fromIndent ind = mconcat $ replicate (ind*4) $ fromChar ' '
+fromIndent ind = mconcat $ replicate (ind*4) " "
